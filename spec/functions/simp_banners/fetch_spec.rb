0a879632c87559ec4d83b74379da77aa07115f5f
Steven Pritchard <steve@sicura.us>
2024-12-17 09:54:54 -0600

    Clean up for rubocop

--- spec/functions/simp_banners/fetch_spec.rb
+++ spec/functions/simp_banners/fetch_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe 'simp_banners::fetch' do
   context 'with known banners' do
-    it { is_expected.to run.with_params('simp').and_return(/ATTENTION/) }
-    it { is_expected.to run.with_params('us/department_of_commerce').and_return(/Department of Commerce/i) }
+    it { is_expected.to run.with_params('simp').and_return(%r{ATTENTION}) }
+    it { is_expected.to run.with_params('us/department_of_commerce').and_return(%r{Department of Commerce}i) }
   end
 
   context 'with an unknown banner' do
-    it { expect{ is_expected.to run.with_params('_nope_') }.to raise_error(/Banner '_nope_' not found/) }
+    it { expect { is_expected.to run.with_params('_nope_') }.to raise_error(%r{Banner '_nope_' not found}) }
   end
 
   context 'format options' do
@@ -16,7 +16,7 @@ describe 'simp_banners::fetch' do
         result = subject.execute('simp', { 'cr_escape' => true })
 
         expect(result.lines.count).to eq(1)
-        expect(result).to match(/ATTENTION.+\\n/)
+        expect(result).to match(%r{ATTENTION.+\\n})
       }
     end
 
@@ -25,13 +25,13 @@ describe 'simp_banners::fetch' do
       it { is_expected.to run.with_params('us/department_of_commerce', { 'file_source' => true }).and_return('puppet:///simp_banners/us/department_of_commerce') }
     end
 
-    it 'should override all other settings' do
+    it 'overrides all other settings' do
       format_options = {
         'file_source' => true,
         'cr_escape'   => true
       }
 
-      is_expected.to run.with_params('simp', format_options ).and_return('puppet:///simp_banners/simp')
+      is_expected.to run.with_params('simp', format_options).and_return('puppet:///simp_banners/simp')
     end
   end
 end
